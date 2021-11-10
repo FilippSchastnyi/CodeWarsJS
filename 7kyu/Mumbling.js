bd46076a94620eccab6a366add2189aed941107b
Pilip.Shchasny <c-pshchasny@neogov.net>
2021-11-10 21:09:02 +0300

    7-kyu-pshchasny-created-the-Mumbling-accum-fu

---
+++ 7kyu/Mumbling.js
@@ -0,0 +1,14 @@
+/*This time no story, no theory. The examples below show you how to write function accum:
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+*/
+
+
+const someWordsForTesting = "ZpglnRxqenU"
+
+const accum = s => s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+
+accum(someWordsForTesting)
+
