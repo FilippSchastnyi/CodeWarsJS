4ae086c13db272893a65a8bd5f9cb8fb5817dca6
Pilip.Shchasny <c-pshchasny@neogov.net>
2021-11-28 00:07:14 +0300

    pshchasny-made-the-highestRank-fu-final

--- 6kuy/Highest-Rank-Number-in-an-Array.js
+++ 6kuy/Highest-Rank-Number-in-an-Array.js
@@ -9,7 +9,7 @@ Complete the method which returns the number which is most frequent in the given
     [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3*/
 
 
-const arrayData = [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]
+const arrayData = [12,10,8,12,7,6,4,10,12,10]
 
 const highestRank = (array) => {
     const store = {}
@@ -21,9 +21,9 @@ const highestRank = (array) => {
         store[item] = array.filter(k => +item === k).length
     })
 
-    return Object.entries(store).reduce((acc, current) => current[1] > acc[1] ? current : acc)[1]
-
+    return +Object.entries(store).reduce((acc, current) => current[1] >= acc[1] ? current : acc)[0]
 }
 
 console.log(highestRank(arrayData))
+
 
